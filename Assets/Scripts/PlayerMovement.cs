490f6427952d67b6e9c88af8e9114eb882b26414
Marcy P <rorypalmer.rp@gmail.com>
2026-02-25 21:08:14 +1100

    Implemented boundaries

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private InputActionReference moveAction;
 
 
+    [SerializeField] private bool useCameraBounds = true;
+    [SerializeField] private Vector2 minBounds;
+    [SerializeField] private Vector2 maxBounds;
+
     private Rigidbody2D rb;
 
     private void Awake()
@@ -38,5 +42,21 @@ public class PlayerMovement : MonoBehaviour
         Vector2 direction = input.normalized;
 
         rb.linearVelocity = direction * speed;
+
+        // Clamp position to bounds
+        Vector3 pos = transform.position;
+        if (useCameraBounds && Camera.main != null)
+        {
+            Vector3 min = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
+            Vector3 max = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, Camera.main.nearClipPlane));
+            pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+            pos.y = Mathf.Clamp(pos.y, min.y, max.y);
+        }
+        else
+        {
+            pos.x = Mathf.Clamp(pos.x, minBounds.x, maxBounds.x);
+            pos.y = Mathf.Clamp(pos.y, minBounds.y, maxBounds.y);
+        }
+        transform.position = pos;
     }
 }
