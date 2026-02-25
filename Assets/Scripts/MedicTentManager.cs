c226fc46179ba2253cb8d11cfe7dcd0f5c266c32
Marcy P <rorypalmer.rp@gmail.com>
2026-02-25 21:22:00 +1100

    soldiers to resucue now updates based on soldiers placed in scene

--- Assets/Scripts/MedicTentManager.cs
+++ Assets/Scripts/MedicTentManager.cs
@@ -5,12 +5,15 @@ public class MedicTentManager : MonoBehaviour
     public GameObject[] droppedOffSoldiers;
     private int droppedOffCount = 0;
 
+    private void Start()
+    {
+        // Set the droppedOffSoldiers array size to the number of soldiers in the scene at start
+        int soldierCount = GameObject.FindGameObjectsWithTag("Soldier").Length;
+        droppedOffSoldiers = new GameObject[soldierCount];
+    }
+
     public bool TryDepositSoldier(GameObject soldier)
     {
-        if (droppedOffSoldiers == null)
-        {
-            droppedOffSoldiers = new GameObject[GameObject.FindGameObjectsWithTag("Soldier").Length];
-        }
         if (droppedOffCount >= droppedOffSoldiers.Length)
             return false;
 
